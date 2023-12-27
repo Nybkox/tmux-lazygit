d30f154178050bfc1e0ef38ab908942ce88fb03a
Nybkox <nybkox@gmail.com>
2023-12-27 16:34:18 +0100

    feat: add keybindings for commands

--- scripts/tmux-lazygit.sh
+++ scripts/tmux-lazygit.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-main() {
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+COMMANDS_FILE="$CURRENT_DIR/commands.sh"
 
+source "$CURRENT_DIR/utils.sh"
+
+main() {
+	tmux set-environment -g TMUX_LAZYGIT_COMMANDS_FILE "$COMMANDS_FILE"
+
+	if [ $(get_tmux_option @tmux-lazygit-default-keybindings true) = "true" ]; then
+		tmux unbind-key g
+		tmux bind-key -r g run-shell "#{TMUX_LAZYGIT_COMMANDS_FILE} toggle_lazygit_window"
+	fi
+
+	bind_if_set @tmux-lazygit-toggle-window-key 'run-shell "#{TMUX_LAZYGIT_COMMANDS_FILE} toggle_lazygit_window"'
 }
 
 main
